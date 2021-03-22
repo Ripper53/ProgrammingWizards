15a220b432e1636db93efdd84ae3bf566dd88dbf
Albar <GalacticHyperStar@gmail.com>
2021-03-22 05:49:13 -0400

    If statements!

---
+++ Examples/RankIfStatements.js
@@ -0,0 +1,13 @@
+let isApprentice = false;
+let isScholar = false;
+let isWizard = false;
+
+if (isApprentice) {
+	console.log("Outside we venture!");
+} else if (isScholar) {
+	console.log("Time to teach!");
+} else if (isWizard) {
+	console.log("Onto expeditions!");
+} else {
+	console.log("More work…");
+}
